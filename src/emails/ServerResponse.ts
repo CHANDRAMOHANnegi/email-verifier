37952244f9730ffe92b1a2f8c4987a9c21bd019e
chandramohan negi <chandramohan@bitcs.in>
2019-07-17 15:03:47 +0530

    adding probability

---
+++ src/emails/ServerResponse.ts
@@ -0,0 +1,23 @@
+class ServerResponse {
+  success: boolean;
+  message: string;
+  value: any;
+  httpCode: number;
+
+  constructor(success: boolean, message: string, value: any, httpCode?: number) {
+    this.success = success;
+    this.message = message;
+    this.value = value;
+    this.httpCode = httpCode;
+  }
+
+  static success(message?: string, value?: any) {
+    return new ServerResponse(true, message, value, 200);
+  }
+
+  static error(message: string, httpCode?: number) {
+    return new ServerResponse(false, message, null, httpCode);
+  }
+}
+
+export default ServerResponse;
